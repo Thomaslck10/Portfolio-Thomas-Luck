f5fb0d80c0746580b62ee051d114cb3d04ad9157
Thomas <thomasluck10@hotmail.fr>
2023-08-12 17:44:12 +0200

    update header + contact

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,15 +7,15 @@ function Header() {
   const links = {Contact : 'Contact', About : 'A propos', Skills : 'Compétences', Works : 'Travaux', Services : 'Services' }
 
   return (
-    <header>
+    <header>    
       <nav>
-      <img src={bar} className="collapse-img" onClick={() => setToggle(!toggle)} alt='icone liste'>
-      </img>
+      <a href='#' className='home'>Thomas Luck</a>  
+      <img src={bar} className="collapse-img" onClick={() => setToggle(!toggle)} alt='icone liste'></img>
+      </nav>
       <div className={toggle ? 'show-content-header' : 'hidden-content'}>
         {Object.keys(links).map(key => 
         <a href={`#${key}`} onClick={() => setToggle(false)}>{links[key]}</a>)}  
       </div>
-      </nav>
     </header>
   );
 }
